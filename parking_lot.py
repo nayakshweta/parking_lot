f73a7e668b656fe569e6429bc8c6675037c238f7
shweta <nayakshweta07@gmail.com>
2019-03-23 21:53:20 +0530

    Adds method to get a new ticket in the parking lot. This finds the closest available empty slot and parks the car there.

--- parking_lot.py
+++ parking_lot.py
@@ -10,3 +10,14 @@ class Parking_Lot:
             slot = Slot(n)
             self.slots.append(slot)
             n += 1
+
+    def get_new_ticket(self, car_number, car_color):
+        n = 1
+        while n <= self.number_of_slots:
+            if self.slots[n - 1].occupied_status == False:
+                closest_empty_slot = self.slots[n - 1]
+                break
+            else:
+                print "No empty slots"
+            n += 1
+        closest_empty_slot.park_car(car_number, car_color)
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from parking_lot import Parking_Lot
+from slot import Slot
 
 class TestParkingLot(TestCase):
 
@@ -9,5 +10,11 @@ class TestParkingLot(TestCase):
 
         assert len(parking_lot.slots) == 6
     
-        
-        
+    def test_get_new_ticket(self):
+        number_of_slots = 6
+        parking_lot = Parking_Lot(number_of_slots)
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
+
+        assert parking_lot.slots[0].car_number == 'KA-03-BB-1234'
+        assert parking_lot.slots[0].car_color == 'White'
+
