f8e9dfa790e54391d5dd33ba152d19e34b02aaba
shweta <nayakshweta07@gmail.com>
2019-03-23 21:02:05 +0530

    Adds method to park a car in a slot while specifying the car number and color.

--- slot.py
+++ slot.py
@@ -3,3 +3,9 @@ class Slot:
     def __init__(self, slot_number):
         self.slot_number = slot_number
         self.occupied_status = False
+    
+    def park_car(self, car_number, car_color):
+        self.car_number = car_number
+        self.car_color = car_color
+        self.occupied_status = True
+    
--- tests/test_slot.py
+++ tests/test_slot.py
@@ -9,3 +9,13 @@ class TestSlot(TestCase):
 
         assert slot.slot_number == 1
         assert slot.occupied_status == False
+    
+    def test_park_car(self):
+        slot_number = 1
+        slot = Slot(slot_number)
+        slot.park_car('KA-03-BB-1234', 'White')
+
+        assert slot.occupied_status == True
+        assert slot.car_number == 'KA-03-BB-1234'
+        assert slot.car_color == 'White'
+        
