859ad9716e86e24ea6aee9774665471a23b6d0ad
shweta <nayakshweta07@gmail.com>
2019-03-23 21:31:27 +0530

    Corrects Parking_Lot class to initialize slot number from 1.

--- parking_lot.py
+++ parking_lot.py
@@ -5,8 +5,8 @@ class Parking_Lot:
     def __init__(self, number_of_slots):
         self.number_of_slots = number_of_slots
         self.slots = []
-        n = 0
-        while n < self.number_of_slots:
+        n = 1
+        while n <= self.number_of_slots:
             slot = Slot(n)
             self.slots.append(slot)
             n += 1
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
 from parking_lot import Parking_Lot
 
-class TestSlot(TestCase):
+class TestParkingLot(TestCase):
 
     def test_initialize_parking_lot(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        
+
         assert len(parking_lot.slots) == 6
+    
+        
+        
