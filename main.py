a12e13d09f7bbd6bf3b054b710f6f00739dbedc7
shweta <nayakshweta07@gmail.com>
2019-03-25 21:56:17 +0530

    Adds main parking lot script. The script uses cmd module to define commands that the script would accept.

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys, cmd
+from parking_lot import Parking_Lot
+from slot import Slot
+
+class ParkingLotShell(cmd.Cmd):
+    intro = "Welcome to the Parking System. Type ? to list commands. \n"
+    prompt = '<parking system>'
+
+    #----- parking lot commands -----#
+    def do_create_parking_lot(self, args):
+        global parking_lot
+        parking_lot = Parking_Lot(args[0])
+    
+    def do_park(self, args):
+        parking_lot.get_new_ticket(args[0], args[1])
+    
+    def do_leave(self, args):
+        parking_lot.return_parking_ticket(args[0])
+    
+    def do_status(self, args):
+        parking_lot.get_parking_status()
+    
+    def do_get_reg_numbers_for_cars_with_color(self, args):
+        parking_lot.get_reg_numbers_for_cars_with_color(args[0])
+
+if __name__ == '__main__':
+    ParkingLotShell().cmdloop()
