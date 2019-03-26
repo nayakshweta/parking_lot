8c094d4606533621416ace9fa07b23ef550ca9b5
shweta <nayakshweta07@gmail.com>
2019-03-26 20:52:12 +0530

    Modifies parking lot instance to be a class variable rather rather than global.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys, cmd
 from parking_lot import Parking_Lot
 from slot import Slot
 
+
+
 class ParkingLotShell(cmd.Cmd):
     intro = "Welcome to the Parking System. Type ? to list commands. \n"
     prompt = '<parking system>'
@@ -11,29 +13,30 @@ class ParkingLotShell(cmd.Cmd):
     else:
         use_rawinput = False
 
+    parking_lot = NotImplemented
+
     #----- parking lot commands -----#
     def do_create_parking_lot(self, args):
-        global parking_lot
-        parking_lot = Parking_Lot(args)
+        self.parking_lot = Parking_Lot(args)
     
     def do_park(self, args):
         args_list = args.split()
-        parking_lot.get_new_ticket(args_list[0], args_list[1])
+        self.parking_lot.get_new_ticket(args_list[0], args_list[1])
     
     def do_leave(self, args):
-        parking_lot.return_parking_ticket(args)
+        self.parking_lot.return_parking_ticket(args)
     
     def do_status(self, args):
-        parking_lot.get_parking_status()
+        self.parking_lot.get_parking_status()
     
     def do_registration_numbers_for_cars_with_colour(self, args):
-        parking_lot.get_reg_numbers_for_cars_with_color(args)
+        self.parking_lot.get_reg_numbers_for_cars_with_color(args)
     
     def do_slot_numbers_for_cars_with_colour(self, args):
-        parking_lot.get_slot_numbers_for_cars_with_color(args)
+        self.parking_lot.get_slot_numbers_for_cars_with_color(args)
 
     def do_slot_number_for_registration_number(self, args):
-        parking_lot.get_slot_number_for_registration_number(args)
+        self.parking_lot.get_slot_number_for_registration_number(args)
 
     def do_EOF(self, line):
         return True
@@ -44,3 +47,4 @@ if __name__ == '__main__':
             ParkingLotShell(stdin=input).cmdloop()
     else:
         ParkingLotShell().cmdloop()
+    
