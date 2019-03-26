93c0a0fab00aee5df6ea712afd482bd289e0573d
shweta <nayakshweta07@gmail.com>
2019-03-26 19:38:35 +0530

    Corrects args usage in the main.py.

--- main.py
+++ main.py
@@ -9,25 +9,26 @@ class ParkingLotShell(cmd.Cmd):
     #----- parking lot commands -----#
     def do_create_parking_lot(self, args):
         global parking_lot
-        parking_lot = Parking_Lot(args[0])
+        parking_lot = Parking_Lot(args)
     
     def do_park(self, args):
-        parking_lot.get_new_ticket(args[0], args[1])
+        args_list = args.split()
+        parking_lot.get_new_ticket(args_list[0], args_list[1])
     
     def do_leave(self, args):
-        parking_lot.return_parking_ticket(args[0])
+        parking_lot.return_parking_ticket(args)
     
     def do_status(self, args):
         parking_lot.get_parking_status()
     
-    def do_get_reg_numbers_for_cars_with_color(self, args):
-        parking_lot.get_reg_numbers_for_cars_with_color(args[0])
+    def do_registration_numbers_for_cars_with_colour(self, args):
+        parking_lot.get_reg_numbers_for_cars_with_color(args)
     
     def do_slot_numbers_for_cars_with_colour(self, args):
-        parking_lot.get_slot_numbers_for_cars_with_color(args[0])
+        parking_lot.get_slot_numbers_for_cars_with_color(args)
 
     def do_slot_number_for_registration_number(self, args):
-        parking_lot.get_slot_number_for_registration_number(args[0])
+        parking_lot.get_slot_number_for_registration_number(args)
 
 if __name__ == '__main__':
     ParkingLotShell().cmdloop()
