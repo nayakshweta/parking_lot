3456ef7b8804d2c9e2ee91c41334a1f22dc381d3
shweta <nayakshweta07@gmail.com>
2019-03-27 23:19:39 +0530

    Refactors class Parking_Lot to allow car as well as bike parking

--- parking_lot.py
+++ parking_lot.py
@@ -4,76 +4,145 @@ class Parking_Lot:
 
     def __init__(self, number_of_slots):
         self.number_of_slots = int(number_of_slots)
-        self.slots = []
+        self.car_slots = []
+        self.bike_slots = []
+
         n = 1
         while n <= self.number_of_slots:
             slot = Slot(n)
-            self.slots.append(slot)
-            n += 1
-        print "Created a parking lot with " + str(self.number_of_slots) + " slots"
-
-
-    def get_new_ticket(self, car_number, car_color):
-        n = 1
-        closest_empty_slot = 0
-        while n <= self.number_of_slots:
-            if self.slots[n - 1].occupied_status == False:
-                closest_empty_slot = self.slots[n - 1]
-                break
+            self.car_slots.append(slot)
             n += 1
         
+        n = 1
+        while n <= self.number_of_slots:
+            slot = Slot(n)
+            self.bike_slots.append(slot)
+            n += 1
+
+        print "Created a parking lot with " + str(self.number_of_slots) + " car and bike slots each"
+
+
+    def find_closest_empty_slot_for_car(self):
+        n = 1
+        closest_empty_slot = 0
+
+        while n <= self.number_of_slots:
+            if self.car_slots[n - 1].occupied_status == False:
+                closest_empty_slot = self.car_slots[n - 1]
+                break
+            n += 1
+        return closest_empty_slot
+
+    def find_closest_empty_slot_for_bike(self):
+        n = 1
+        closest_empty_slot = 0
+
+        while n <= self.number_of_slots:
+            if self.bike_slots[n - 1].occupied_status == False:
+                closest_empty_slot = self.bike_slots[n - 1]
+                break
+            n += 1
+        return closest_empty_slot
+
+
+    def get_new_ticket(self, vehicle_number, vehicle_color, vehicle_type):
+
+        if vehicle_type == 'Car':
+            closest_empty_slot = self.find_closest_empty_slot_for_car()
+        elif vehicle_type == 'Bike':
+            closest_empty_slot = self.find_closest_empty_slot_for_bike()
+
         if not closest_empty_slot:
             print "Sorry, parking lot is full!"
         else:
             print "Allocated slot number: " + str(closest_empty_slot.slot_number)
-            closest_empty_slot.park_car(car_number, car_color)
-        
+            closest_empty_slot.park_vehicle(vehicle_number, vehicle_color)
+
 
     def get_parking_status(self):
+
+        print "Car Parking"
+        print "==========="
         print "\nSlot No.\tRegistration Number\tColor"
-        for slot in self.slots:
+        for slot in self.car_slots:
             if slot.occupied_status == True:
-                print str(slot.slot_number) + '\t\t' + slot.car_number + '\t\t' + slot.car_color
-    
+                print str(slot.slot_number) + '\t\t' + slot.vehicle_number + '\t\t' + slot.vehicle_color
+        
+        print "Bike Parking"
+        print "============"
+        print "\nSlot No.\tRegistration Number\tColor"
+        for slot in self.bike_slots:
+            if slot.occupied_status == True:
+                print str(slot.slot_number) + '\t\t' + slot.vehicle_number + '\t\t' + slot.vehicle_color
 
-    def return_parking_ticket(self, slot_number):
+
+    def return_parking_ticket(self, slot_number, vehicle_type):
         slot_number = int(slot_number)
-        slot = self.slots[slot_number - 1]
-        slot.exit_car()
-        print "Slot Number " + str(slot_number) + " is free."
-    
 
-    def get_reg_numbers_for_cars_with_color(self, car_color):
+        if vehicle_type == 'Car':
+            slot = self.car_slots[slot_number - 1]
+        elif vehicle_type == 'Bike':
+            slot = self.bike_slots[slot_number - 1]
+
+        slot.exit_vehicle()
+        print "Slot Number " + str(slot_number) + " " + vehicle_type + " is free."
+
+
+    def get_reg_numbers_for_vehicles_with_color(self, vehicle_color):
         list_of_cars = []
+        list_of_bikes = []
 
-        for slot in self.slots:
-            if (slot.occupied_status == True) and (slot.car_color == car_color):
-                list_of_cars.append(slot.car_number)
-        print ', '.join(list_of_cars)
+        for slot in self.car_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_color == vehicle_color):
+                list_of_cars.append(slot.vehicle_number)
+        print "The cars with color " + vehicle_color + " are: " + ', '.join(list_of_cars)
+
+        for slot in self.bike_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_color == vehicle_color):
+                list_of_bikes.append(slot.vehicle_number)
+        print "The bikes with color " + vehicle_color + " are: " + ', '.join(list_of_bikes)
 
         if not list_of_cars:
-            print "Not Found"
+            print "No Cars Found"
+        if not list_of_bikes:
+            print "No Bikes Found"
 
 
-    def get_slot_numbers_for_cars_with_color(self, car_color):
+    def get_slot_numbers_for_vehicles_with_color(self, vehicle_color):
         list_of_cars = []
+        list_of_bikes = []
 
-        for slot in self.slots:
-            if (slot.occupied_status == True) and (slot.car_color == car_color):
+        for slot in self.car_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_color == vehicle_color):
                 list_of_cars.append(str(slot.slot_number))
-        print ', '.join(list_of_cars)
+        print "The cars with color " + vehicle_color + " are at the slots: " +', '.join(list_of_cars)
+
+        for slot in self.bike_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_color == vehicle_color):
+                list_of_bikes.append(str(slot.slot_number))
+        print "The bikes with color " + vehicle_color + " are at the slots: " +', '.join(list_of_bikes)
 
         if not list_of_cars:
-            print "Not Found"
+            print "No Cars Found"
+        if not list_of_bikes:
+            print "No Bikes Found"
 
 
-    def get_slot_number_for_registration_number(self, car_number):
+    def get_slot_number_for_registration_number(self, vehicle_number):
         list_of_cars = []
+        list_of_bikes = []
 
-        for slot in self.slots:
-            if (slot.occupied_status == True) and (slot.car_number == car_number):
+        for slot in self.car_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_number == vehicle_number):
                 list_of_cars.append(str(slot.slot_number))
-        print ', '.join(list_of_cars)
+        print "Car Parking: " + ', '.join(list_of_cars)
+
+        for slot in self.bike_slots:
+            if (slot.occupied_status == True) and (slot.vehicle_number == vehicle_number):
+                list_of_bikes.append(str(slot.slot_number))
+        print "Bike Parking: " + ', '.join(list_of_bikes)
 
         if not list_of_cars:
-            print "Not Found"
+            print "No Cars Found"
+        if not list_of_bikes:
+            print "No Bikes Found"
--- tests/test_parking_lot.py
+++ tests/test_parking_lot.py
@@ -8,57 +8,77 @@ class TestParkingLot(TestCase):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
 
-        assert len(parking_lot.slots) == 6
+        assert len(parking_lot.car_slots) == 6
+        assert len(parking_lot.bike_slots) == 6
     
+    def test_find_closest_empty_slot_for_car(self):
+        number_of_slots = 6
+        parking_lot = Parking_Lot(number_of_slots)
+        closest_empty_slot = parking_lot.find_closest_empty_slot_for_car()
+
+        assert closest_empty_slot.slot_number == 1
+
+    def test_find_closest_empty_slot_for_bike(self):
+        number_of_slots = 6
+        parking_lot = Parking_Lot(number_of_slots)
+        parking_lot.bike_slots[0].occupied_status = True
+        parking_lot.bike_slots[1].occupied_status = True
+        closest_empty_slot = parking_lot.find_closest_empty_slot_for_bike()
+
+        assert closest_empty_slot.slot_number == 3
+
     def test_get_new_ticket(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
 
-        assert parking_lot.slots[0].car_number == 'KA-03-BB-1234'
-        assert parking_lot.slots[0].car_color == 'White'
+        assert parking_lot.car_slots[0].vehicle_number == 'KA-03-BB-1234'
+        assert parking_lot.car_slots[0].vehicle_color == 'White'
+        assert parking_lot.car_slots[0].occupied_status == True
+
+        assert parking_lot.bike_slots[0].occupied_status == False
 
     def test_get_parking_status(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
-        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red')
-        parking_lot.get_new_ticket('KA-03-BB-1245', 'White')
-        parking_lot.get_new_ticket('KA-03-BB-1780', 'Black')
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
+        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red', 'Bike')
+        parking_lot.get_new_ticket('KA-03-BB-1245', 'White', 'Bike')
+        parking_lot.get_new_ticket('KA-03-BB-1780', 'Black', 'Car')
         parking_lot.get_parking_status()
     
     def test_return_parking_ticket(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
-        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red')
-        parking_lot.return_parking_ticket(1)
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
+        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red', 'Bike')
+        parking_lot.return_parking_ticket(1, 'Car')
 
-        assert parking_lot.slots[0].car_number == 'None'
-        assert parking_lot.slots[0].car_color == 'None'
-        assert parking_lot.slots[0].occupied_status == False
+        assert parking_lot.car_slots[0].vehicle_number == 'None'
+        assert parking_lot.car_slots[0].vehicle_color == 'None'
+        assert parking_lot.car_slots[0].occupied_status == False
     
-    def test_get_reg_numbers_for_cars_with_color(self):
+    def test_get_reg_numbers_for_vehicles_with_color(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
-        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red')
-        parking_lot.get_new_ticket('KA-03-BB-1245', 'White')
-        parking_lot.get_reg_numbers_for_cars_with_color('White')
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
+        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red', 'Bike')
+        parking_lot.get_new_ticket('KA-03-BB-1245', 'White', 'Bike')
+        parking_lot.get_reg_numbers_for_vehicles_with_color('White')
     
     def test_get_slot_numbers_of_cars_with_color(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
-        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red')
-        parking_lot.get_new_ticket('KA-03-BB-1245', 'White')
-        parking_lot.get_slot_numbers_for_cars_with_color('White')
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
+        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red', 'Bike')
+        parking_lot.get_new_ticket('KA-03-BB-1245', 'White', 'Bike')
+        parking_lot.get_slot_numbers_for_vehicles_with_color('White')
 
     def test_get_slot_number_for_registration_number(self):
         number_of_slots = 6
         parking_lot = Parking_Lot(number_of_slots)
-        parking_lot.get_new_ticket('KA-03-BB-1234', 'White')
-        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red')
-        parking_lot.get_new_ticket('KA-03-BB-1245', 'White')
+        parking_lot.get_new_ticket('KA-03-BB-1234', 'White', 'Car')
+        parking_lot.get_new_ticket('KA-03-BH-1260', 'Red', 'Bike')
+        parking_lot.get_new_ticket('KA-03-BB-1245', 'White', 'Bike')
         parking_lot.get_slot_number_for_registration_number('KA-03-BH-1260')
     
